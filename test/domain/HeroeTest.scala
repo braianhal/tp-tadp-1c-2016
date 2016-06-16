0fedbf1ae8067d38f129c9cae9251d4d6445d6ef
EricKalinowski <eric_manutd7@hotmail.es>
2016-06-16 02:50:04 -0300

    Test de los stats, y probando que no quedan con stats negativos

--- test/domain/HeroeTest.scala
+++ test/domain/HeroeTest.scala
@@ -34,15 +34,23 @@ class HeroeTest {
   
   @Test
   def aumentarVelocidadDeHeroe() = {
-    val heroeMasFuerte:Heroe = heroe.modificarStats((Velocidad,20))
+    val heroeMasVeloz:Heroe = heroe.modificarStats((Velocidad,20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
-    assertEquals(heroeMasFuerte.statsBase, Stats(10,20,50,40))
+    assertEquals(heroeMasVeloz.statsBase, Stats(10,20,50,40))
   }
   
   @Test
   def aumentarInteligenciaDeHeroe() = {
-    val heroeMasFuerte:Heroe = heroe.modificarStats((Inteligencia,20))
+    val heroeMasInteligente:Heroe = heroe.modificarStats((Inteligencia,20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
-    assertEquals(heroeMasFuerte.statsBase, Stats(10,20,30,60))
+    assertEquals(heroeMasInteligente.statsBase, Stats(10,20,30,60))
+  }
+  
+  @Test
+  def heroeNoAceptaStatsNegativos() = {
+    
+    val heroeMenosFuerte:Heroe = heroe.modificarStats((HP,-15420),(Fuerza,-77777720),(Velocidad,-1520),(Inteligencia,-200))
+    assertEquals(heroe.statsBase, Stats(10,20,30,40))
+    assertEquals(heroeMenosFuerte.statsBase, Stats(1,1,1,1))
   }
 }
