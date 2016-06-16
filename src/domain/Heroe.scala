2f5aa63475993abd684593251b72aeb243817da4
braianhal <braianhal@gmail.com>
2016-06-15 23:54:09 -0300

    agregado stats

--- src/domain/Heroe.scala
+++ src/domain/Heroe.scala
@@ -1,5 +1,31 @@
 package domain
 
-case class Heroe() {
+case class Heroe(statsBase:Stats) {
+  
+  def modificarStats(variaciones:(Stat,Int)*) = {
+    copy(statsBase.actualizarSegun(variaciones.toList))
+  }
   
 }
+
+case class Stats(hp:Int,fuerza:Int,velocidad:Int,inteligencia:Int){
+  
+  def actualizarSegun(variaciones:List[(Stat,Int)]) = {
+    var nuevosStats:Stats = copy()
+    variaciones.foreach { variacion => 
+      variacion match {
+        case (HP,x) => nuevosStats = nuevosStats.copy(hp = actualizarStat(this.hp,x))
+        case (Fuerza,x) => nuevosStats = nuevosStats.copy(fuerza = Math.max(1,x + this.hp))
+      }
+    }
+    nuevosStats
+  }
+  
+  def actualizarStat(valorAnterior:Int,variacion:Int) = Math.max(1,variacion + valorAnterior)
+}
+
+class Stat
+case object HP extends Stat
+case object Fuerza extends Stat
+case object Velocidad extends Stat
+case object Inteligencia extends Stat
--- test/domain/HeroeTest.scala
+++ test/domain/HeroeTest.scala
@@ -1,5 +1,27 @@
 package domain
 
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert._
+
 class HeroeTest {
   
+  var heroe:Heroe = null
+  
+  @Before
+  def setup() = {
+     heroe = Heroe(Stats(10,10,20,30))
+  }
+
+  @Test
+  def heroeSeCreaConStatsBase() = {
+    assertEquals(heroe.statsBase, Stats(10,10,20,30))
+  }
+  
+  @Test
+  def aumentarFuerzaDeHeroe() = {
+    val heroeMasFuerte:Heroe = heroe.modificarStats((Fuerza,20))
+    assertEquals(heroe.statsBase, Stats(10,10,20,30))
+    assertEquals(heroeMasFuerte.statsBase, Stats(10,30,20,30))
+  }
 }
