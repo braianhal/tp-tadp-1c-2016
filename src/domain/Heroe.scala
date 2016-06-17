fececa1e71bcf8fbd0d33c7de7a3675697f726ea
braianhal <braianhal@gmail.com>
2016-06-16 23:21:44 -0300

    cambio en stats y agregado item/inventario/trabajo

--- src/domain/Heroe.scala
+++ src/domain/Heroe.scala
@@ -3,28 +3,33 @@ package domain
 case class Heroe(statsBase:Stats) {
   
   val inventario:Inventario = Inventario()
-  val trabajo:Trabajo = SinTrabajo
+  val trabajo:Trabajo = null
+
   
-  def modificarStats(variaciones:(Stat,Int)*) = {
+  def modificarStats(variaciones:Stat*) = {
     copy(statsBase.actualizarSegun(variaciones.toList))
   }
   
-  def stats(){
-    trabajo.modificarStats(statsBase)
+  def equipar(item:Item) {
+    if(item.cumpleCondicion(this)){
+      inventario.agregar(item)
+    }
   }
+  
+  
   
 }
 
 case class Stats(hp:Int,fuerza:Int,velocidad:Int,inteligencia:Int){
   
-  def actualizarSegun(variaciones:List[(Stat,Int)]) = {
+  def actualizarSegun(variaciones:List[Stat]) = {
     var nuevosStats:Stats = copy()
     variaciones.foreach { variacion => 
       variacion match {
-        case (HP,x) => nuevosStats = nuevosStats.copy(hp = actualizarStat(this.hp,x))
-        case (Fuerza,x) => nuevosStats = nuevosStats.copy(fuerza = actualizarStat(this.fuerza,x))
-        case (Velocidad,x) => nuevosStats = nuevosStats.copy(velocidad = actualizarStat(this.velocidad,x))
-        case (Inteligencia,x) => nuevosStats = nuevosStats.copy(inteligencia = actualizarStat(this.inteligencia,x))
+        case HP(x) => nuevosStats = nuevosStats.copy(hp = actualizarStat(this.hp,x))
+        case Fuerza(x) => nuevosStats = nuevosStats.copy(fuerza = actualizarStat(this.fuerza,x))
+        case Velocidad(x) => nuevosStats = nuevosStats.copy(velocidad = actualizarStat(this.velocidad,x))
+        case Inteligencia(x) => nuevosStats = nuevosStats.copy(inteligencia = actualizarStat(this.inteligencia,x))
       }
     }
     nuevosStats
@@ -35,14 +40,38 @@ case class Stats(hp:Int,fuerza:Int,velocidad:Int,inteligencia:Int){
 
 case class Inventario(){
   
+  val items:List[Item] = List()
+  
+  def agregar(item:Item) {
+    
+  }
+  
 }
 
-abstract class Trabajo(statPrincipal:Stat,variacionStats:(Stat,Int)*)
+class Trabajo(statPrincipal:Stat,stats:List[Stat]){
+  
+  
+  
+}
 
-case class SinTrabajo() extends Trabajo(HP)
+abstract class Stat(valor:Int)
+case class HP(valor:Int) extends Stat(valor)
+case class Fuerza(valor:Int) extends Stat(valor)
+case class Velocidad(valor:Int) extends Stat(valor)
+case class Inteligencia(valor:Int) extends Stat(valor)
 
-class Stat
-case object HP extends Stat
-case object Fuerza extends Stat
-case object Velocidad extends Stat
-case object Inteligencia extends Stat
+
+
+case class Item(tipo:TipoItem,efectoSobreHeroe:(Heroe => List[(Stat,Int)]),condicion:(Heroe => Boolean)) {
+  
+  def cumpleCondicion(heroe:Heroe):Boolean = {
+    condicion(heroe)
+  }
+  
+}
+
+trait TipoItem
+case object Cabeza
+case object Torso
+case class Mano(cantidad:Int)
+case object Talisman
--- test/domain/HeroeTest.scala
+++ test/domain/HeroeTest.scala
@@ -20,28 +20,28 @@ class HeroeTest {
   
   @Test
   def aumentarHpDeHeroe() = {
-    val heroeMasFuerte:Heroe = heroe.modificarStats((HP,20))
+    val heroeMasFuerte:Heroe = heroe.modificarStats(HP(20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
     assertEquals(heroeMasFuerte.statsBase, Stats(30,20,30,40))
   }
   
   @Test
   def aumentarFuerzaDeHeroe() = {
-    val heroeMasFuerte:Heroe = heroe.modificarStats((Fuerza,20))
+    val heroeMasFuerte:Heroe = heroe.modificarStats(Fuerza(20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
     assertEquals(heroeMasFuerte.statsBase, Stats(10,40,30,40))
   }
   
   @Test
   def aumentarVelocidadDeHeroe() = {
-    val heroeMasVeloz:Heroe = heroe.modificarStats((Velocidad,20))
+    val heroeMasVeloz:Heroe = heroe.modificarStats(Velocidad(20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
     assertEquals(heroeMasVeloz.statsBase, Stats(10,20,50,40))
   }
   
   @Test
   def aumentarInteligenciaDeHeroe() = {
-    val heroeMasInteligente:Heroe = heroe.modificarStats((Inteligencia,20))
+    val heroeMasInteligente:Heroe = heroe.modificarStats(Inteligencia(20))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
     assertEquals(heroeMasInteligente.statsBase, Stats(10,20,30,60))
   }
@@ -49,7 +49,7 @@ class HeroeTest {
   @Test
   def heroeNoAceptaStatsNegativos() = {
     
-    val heroeMenosFuerte:Heroe = heroe.modificarStats((HP,-15420),(Fuerza,-77777720),(Velocidad,-1520),(Inteligencia,-200))
+    val heroeMenosFuerte:Heroe = heroe.modificarStats(HP(-15420),Fuerza(-77777720),Velocidad(-1520),Inteligencia(-200))
     assertEquals(heroe.statsBase, Stats(10,20,30,40))
     assertEquals(heroeMenosFuerte.statsBase, Stats(1,1,1,1))
   }
