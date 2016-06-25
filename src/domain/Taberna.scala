ab2b6817a0a936659ebf7aea8b5e7e80493c6c5f
Leonardo-Rocca <leonardo_7@live.com.ar>
2016-06-24 22:09:55 -0300

    fix elegir mision}

--- src/domain/Taberna.scala
+++ src/domain/Taberna.scala
@@ -2,7 +2,7 @@ package domain
 
 class Taberna(misiones:List[Mision]) {
   
-  def elegirMision(equipo:Equipo, criterio:((Equipo,Equipo)=>Boolean),mision1:Mision,mision2:Mision):Option[Equipo] = {
+  def elegirMision(equipo:Equipo, criterio:((Equipo,Equipo)=>Boolean),mision1:Mision,mision2:Mision):Option[Mision] = {
     val e1 = mision1.serRealizadaPor(equipo)
     val e2 = mision1.serRealizadaPor(equipo)
 
@@ -16,17 +16,19 @@ class Taberna(misiones:List[Mision]) {
 	  }
     
     (r1,r2)match{
-      case (Some(x),None) => Some(x)
-      case (None,Some(x)) => Some(x)
-      case (Some(x),Some(y)) => Some(this.elMejor(x,y,criterio))
+      case (Some(x),None) => Some(mision1)
+      case (None,Some(x)) => Some(mision2)
+      case (Some(x),Some(y)) => Some(this.elMejor(x,mision1,y,mision2,criterio))
       case (None,None) => None
     }
   }
 
-  def elMejor(x: Equipo, y: Equipo,criterio:((Equipo,Equipo)=>Boolean)) = {
+  def elMejor(x: Equipo, m1: Mision, y: Equipo,m2: Mision,criterio:((Equipo,Equipo)=>Boolean)):Mision = {
     if (criterio(x,y))
-    x
-    else y
+    m1
+    else 
+    m2
   }
+
   
 }
